bcc20a2ff3ef7225ff94b8dcb117add36be20eca
anupamroy8 <anupamroy8@gmail.com>
2020-03-02 14:03:59 +0530

    update website address

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Anupam Roy',
   githubUrl: 'https://github.com/',
   // replace this by the url where your website will be published
-  siteUrl: 'anupamroy.codes',
+  siteUrl: 'https://anupamroy.codes',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
